4a15612f8f1f27f8dad9eabfc7eca9579641e275
ogQoda <carlos_ojr@live.com>
2023-05-15 17:44:40 -0300

    Implementação das classes Bootcamp e Dev

--- src/br/com/acme/desafio/dominio/Dev.java
+++ src/br/com/acme/desafio/dominio/Dev.java
@@ -1,4 +1,52 @@
 package br.com.acme.desafio.dominio;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+
+    }
+
+    public void progredir(){
+
+    }
+
+    public void calcularTotalXp(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
 }
